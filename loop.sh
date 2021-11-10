3c6f4192a10691e306cefb3e3f91e026f1756f33
Sudhir Yadav <sudhiryadav.igi@gmail.com>
2021-11-10 06:57:23 +0530

    Fix Looper
    * Unable to commit.

--- loop.sh
+++ loop.sh
@@ -14,12 +14,12 @@ if [ -f $(pwd)/rom/out/target/product/${T_DEVICE}/${ZIPNAME} ]; then
        telegram_message "
               	*✅ Build Finished No Loop Required ✅*"  &> /dev/null
 else
-       git clone https://github.com/TheSanty/GitLol loop
+       git clone https://github.com/TheSanty/fuck loop
        cd loop || exit 1
        echo $(( RANDOM - ( RANDOM % RANDOM ) )) > builds.txt
        git add builds.txt
        git commit -m "Build : Loop"
-       git push -q https://${TOKEN}@github.com/TheSanty/GitLol HEAD:main
+       git push -q https://${TOKEN}@github.com/TheSanty/fuck HEAD:master
        telegram_message "
               	*🌟 $T_ROM Build Loop 🌟*"  &> /dev/null
 fi
